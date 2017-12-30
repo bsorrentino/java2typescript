6a6840727ae69343ef91a74dd144cffd7e7ec911
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-12-30 19:02:19 +0100

    update sample

---
+++ jjs/src/main/ts/jdk8-types.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ArrayList = Java.type("java.util.ArrayList");
---
+++ jjs/src/main/ts/jdk8-types.ts
@@ -0,0 +1,6 @@
+
+interface ArrayListStatic {
+  new<T>():java.util.ArrayList<T>;
+}
+
+export var ArrayList:  ArrayListStatic = Java.type( "java.util.ArrayList");
