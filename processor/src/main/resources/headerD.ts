58c44a856dcc7b26e487574ca95e4af214555f87
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-12-30 23:54:53 +0100

    export factory method

--- processor/src/main/resources/headerD.ts
+++ processor/src/main/resources/headerD.ts
@@ -14,58 +14,13 @@ type bytearray = [char];
 
 type Runnable = () => void;
 
-interface Supplier<T> {
-	():T; //get():T
-}
+type Supplier<T> = () => T;
 
-interface Consumer<T> /*extends Function*/ {
-	andThen?( arg0:Consumer<T> ):Consumer<T>;
-	( v:T ):void; //accept(t:T):void
-}
+type Consumer<T> = ( v:T) => void;
 
-interface ConsumerConstructor {
-    new<T>( args: T): Consumer<T>;
-    <T>( args: T): Consumer<T>;
-    readonly prototype: Function;
-}
+type UnaryOperator<T> = ( v:T ) => T ;
 
-declare const Consumer : ConsumerConstructor;
-
-interface UnaryOperator<T>/* extends Function<T,any>*/ {
-
-	andThen?( arg0:any /* java.util.function.Function */ ):any /* java.util.function.Function */;
-	compose?( arg0:any /* java.util.function.Function */ ):any /* java.util.function.Function */;
-	( v:T ):T //apply(t:T):T
-
-}
-
-interface UnaryOperatorConstructor {
-    new<T>( args: T): UnaryOperator<T>;
-    <T>( args: T): UnaryOperator<T>;
-    /*static */identity<T>(  ):UnaryOperator<T>;
-    readonly prototype: Function;
-}
-
-declare const UnaryOperator : UnaryOperatorConstructor;
-
-interface Predicate<T>  {
-
-	and?( arg0:Predicate<T> ):Predicate<T>;
-	negate?():Predicate<T>;
-	or?( arg0:Predicate<T> ):Predicate<T>;
-	( v:T ):boolean //test( arg0:T /* java.lang.Object */ ):boolean;
-
-}
-
-interface PredicateConstructor {
-    new<T>( args: T): UnaryOperator<T>;
-    <T>( args: T): UnaryOperator<T>;
-
-	/*static*/ isEqual<T>( arg0:any /* java.lang.Object */ ):Predicate<T>;
-
-}
-
-declare const Predicate : PredicateConstructor;
+type Predicate<T>  = ( v:T ) => boolean;
 
 declare namespace java.lang {
 
@@ -91,20 +46,4 @@ declare namespace java.io {
 
 	interface Closeable {}
 	interface Serializable {}
-}
-
-declare namespace java.util.stream {
-
-	interface StreamConstructor{
-		builder<T>(  ):Stream.Builder<T>;
-		concat<T>( arg0:Stream<T>,arg1:Stream<T> ):Stream<T>;
-		empty<T>(  ):Stream<T>;
-		generate<T>( arg0:Supplier<T> ):Stream<T>;
-		iterate<T>( seed:T,arg1:UnaryOperator<T> ):Stream<T>;
-		of<T>( arg0:Array<T> ):Stream<T>;
-		of<T>( arg0:T ):Stream<T>;
-
-	}
-
-	export const Stream : java.util.stream.StreamConstructor;
 }
