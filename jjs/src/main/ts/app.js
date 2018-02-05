9a9a763031a56148a4742040257f8215ccd61f11
bsorrentino <bartolomeo.sorrentino@gmail.com>
2018-02-05 14:02:38 -0600

    update pom

--- jjs/src/main/ts/app.js
+++ jjs/src/main/ts/app.js
@@ -9,7 +9,9 @@ java.lang.System.setProperty( "jvm-npm.debug", "true");
 // PLOYFILL
 //The $ENV variable provides the shell environment variables.
 //The $ARG variable is an array of the program command-line arguments.
-var process = { argv:"", env:{TERM:'color'} } ;
+print( "args", $ARG.length );
+
+var process = { argv:$ARGS, env:{TERM:'color'} } ;
 
 var exports = {};
 load('./main.js');
