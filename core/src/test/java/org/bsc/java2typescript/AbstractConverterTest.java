34124ec4c9e13f7ccd6ac79030c23d5bf019738c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2018-05-16 17:25:40 +0200

    support of declarative member classes #2

---
+++ core/src/test/java/org/bsc/java2typescript/AbstractConverterTest.java
@@ -0,0 +1,46 @@
+package org.bsc.java2typescript;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public abstract class AbstractConverterTest {
+    
+    final TypescriptConverter converter = new TypescriptConverter();
+    
+    protected java.util.Map<String,TSType> declaredClassMap( Class<?> ... classes) {
+        return Stream.of( classes )
+            .collect( Collectors.toMap( c -> c.getName(), c -> TSType.from(c) ))
+            ;       
+    }
+    protected java.util.Map<String,TSType> declaredTypeMap( TSType ... types) {
+        return Stream.of( types )
+            .collect( Collectors.toMap( t -> t.getValue().getName(), t -> t ))
+            ;       
+    }
+    
+    protected String getReturnType( Class<?> type, String methonName, Class<?> ...args ) throws Exception {
+        return getReturnType(Collections.emptyMap(), type, methonName, (Class<?>[])args);
+    }
+    
+    protected String getReturnType( java.util.Map<String, TSType> declaredClassMap, Class<?> type, String methodName, Class<?> ...args ) throws Exception 
+    {
+        final Method m = type.getMethod(methodName, (Class<?>[])args);          
+        return getReturnType( declaredClassMap, type, m);
+    }
+    
+    protected String getReturnType( java.util.Map<String, TSType> declaredClassMap, Class<?> type, Method m ) throws Exception 
+    {
+        final Type rType = m.getGenericReturnType();
+        final String result = TypescriptConverter.convertJavaToTS(rType, m, 
+                TSType.from(type),
+                declaredClassMap, 
+                true, 
+                Optional.empty());
+        return result;
+    }
+
+}
