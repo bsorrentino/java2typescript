13bb09e199909b2e4045b92cc13b823e8eed831b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2019-06-13 01:39:01 +0200

    add support for 'jvm-npm'

--- samples/app.js
+++ samples/app.js
@@ -2,6 +2,16 @@
 // STARTUP
 //
 
+load('./target/js/jvm-npm.js');
+
+print( typeof require );
+
+require.paths = [
+  "node_emul"
+];
+
+java.lang.System.setProperty( 'jvm-npm.debug', 'false');
+
 var process = {
   argv:'',
   platform:'macosx',
